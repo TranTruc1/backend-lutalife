c68c9c5397763f3b9cdcd4d14b8f19cc4121e996
TranTruc1 <96124416+TranTruc1@users.noreply.github.com>
2025-09-06 10:12:03 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,26 +1,4 @@
-import express from "express";
+import app from "../server.js";
 import serverless from "serverless-http";
-import mongoose from "mongoose";
 
-import authRoutes from "../routes/auth.js";
-import userRoutes from "../routes/users.js";
-import appointmentRoutes from "../routes/appointmentRoutes.js";
-import customerRoutes from "../routes/customers.js";
-
-const app = express();
-app.use(express.json());
-
-// DB connect
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// routes
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/appointments", appointmentRoutes);
-app.use("/api/customers", customerRoutes);
-
-// 👇 cái này là bắt buộc để vercel chạy
 export default serverless(app);
